f4f17268624d03520170ffa1ba830e766b250b50
Sebastian Flores <sebasflores992@gmail.com>
2026-09-21 13:55:14 -0600

    feat: create Realm of Ashes React base

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config'
+export default defineConfig([
+  { ignores: ['dist'] },
+  { files: ['**/*.{js,jsx}'], extends: [js.configs.recommended, reactHooks.configs['recommended-latest']], languageOptions: { ecmaVersion: 2022, globals: globals.browser, parserOptions: { ecmaFeatures: { jsx: true }, sourceType: 'module' } }, plugins: { 'react-refresh': reactRefresh }, rules: { 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }] } }
+])
